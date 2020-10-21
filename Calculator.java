7d4cf0723bb9e8843b38f855b359b6198d85e226
ShannonLoughin <loughins@dickinson.edu>
2020-10-20 20:21:40 -0400

    faked pull request

--- Calculator.java
+++ Calculator.java
@@ -58,7 +58,7 @@ public class Calculator {
 	 *            a double
 	 * @return x/y
 	 */
-	public double divide(double x, double y) {
+	public double doDivisionide(double x, double y) {
 		return y / x;
 	}
 
